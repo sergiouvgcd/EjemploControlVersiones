245a5bebbc9f8d65ab27ec244699cb5ec3eaf307
Sergiouvgcd <delcarmo@alumni.uv>
2025-02-07 23:26:50 +0100

    Perímetro rectángulo y cambio PI’

---
+++ Perim.R
@@ -0,0 +1,10 @@
+longitud_circunferencia <- function(radio) {
+  # Fórmula: Longitud = 2 * 3.14159 * radio
+  longitud <- 2 * 3.14159 * radio
+  return(longitud)
+}
+longitud_rectangulo <- function(l1,l2) {
+  # Fórmula: Longitud = 2 * 3.14159 * radio
+  longitud <- l1*l2
+  return(longitud)
+}
